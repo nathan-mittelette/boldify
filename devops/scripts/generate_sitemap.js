b874e0ee072792b15ed2e69b76d89bbb651a0274
Nathan Mittelette <nathan.mittelette@axopen.com>
2026-01-01 12:38:33 +0100

    feat: upgrade sitemap.xml

--- devops/scripts/generate_sitemap.js
+++ devops/scripts/generate_sitemap.js
@@ -25,12 +25,13 @@ const today = new Date().toISOString().split('T')[0];
 const generateSitemap = () => {
 	const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${routes
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">${routes
 		.map(
 			({ loc, priority }) => `
 		<url>
 				<loc>${loc}</loc>
 				<lastmod>${today}</lastmod>
+				<changefreq>daily</changefreq>
 				<priority>${priority}</priority>
 		</url>`
 		)
